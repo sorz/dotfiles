39f9f9ebfa4b5098070189cfed3e5b003e7b081e
sorz <me@sorz.org>
2019-07-08 02:09:35 +0800

    Overwrite existing files when linking

--- install.sh
+++ install.sh
@@ -3,16 +3,16 @@ cd ~/.dotfiles
 git pull
 
 # Git
-ln -sv ~/.dotfiles/git/.gitconfig ~
-ln -sv ~/.dotfiles/git/.gitattributes ~
+ln -svf ~/.dotfiles/git/.gitconfig ~
+ln -svf ~/.dotfiles/git/.gitattributes ~
 
 # Mintty
 if [ "$TERM" = "xterm" ]; then
-    ln -sv ~/.dotfiles/mintty/.minttyrc ~
+    ln -svf ~/.dotfiles/mintty/.minttyrc ~
 fi
 
 # Bash
-ln -sv ~/.dotfiles/bash/.bashrc ~
+ln -svf ~/.dotfiles/bash/.bashrc ~
 BASHD=~/.dotfiles/bash/generated
 mkdir -p $BASHD
 if hash pip 2> /dev/null; then
@@ -22,16 +22,16 @@ fi
 # GnuPG
 mkdir -p ~/.gnupg
 chmod 700 ~/.gnupg
-ln -sv ~/.dotfiles/gnupg/gpg-agent.conf ~/.gnupg/
-ln -sv ~/.dotfiles/gnupg/gpg.conf ~/.gnupg/
+ln -svf ~/.dotfiles/gnupg/gpg-agent.conf ~/.gnupg/
+ln -svf ~/.dotfiles/gnupg/gpg.conf ~/.gnupg/
 
 # Byobu
 if [ -d "$HOME/.byobu" ]; then
-    ln -sv ~/.dotfiles/byobu/.tmux.conf ~/.byobu/
+    ln -svf ~/.dotfiles/byobu/.tmux.conf ~/.byobu/
 fi
 
 # Vim
-ln -sv ~/.dotfiles/vim/.vimrc ~
+ln -svf ~/.dotfiles/vim/.vimrc ~
 mkdir -p -m 700 ~/.cache
 mkdir -p -m 700 ~/.cache/vim
 if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
