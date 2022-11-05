6103e09ea2a5acc40145d98fb9225313f66e0c74
Shell Chen <me@sorz.org>
2022-11-06 00:52:25 +0800

    Neovim: fix cmp_nvim_lsp warning

--- nvim/init.lua
+++ nvim/init.lua
@@ -126,7 +126,8 @@ cmp.setup {
     }),
 }
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp')
+    .default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lsp_flags = {
     debounce_text_changes = 150,
 }
