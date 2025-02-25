3be08a7446b7feb858da4c828d6d35040ec76eff
Shell Chen <me@sorz.org>
2025-02-25 11:41:31 +0000

    Fish: sync history & add abbr/alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,29 @@
 if status is-interactive
-    # ...
+    # Sync history across sessions
+    # https://github.com/fish-shell/fish-shell/issues/825
+    bind \e\[A 'history merge; up-or-search'
+
+    # Abbr
+    if command -q systemctl
+        abbr -a sys sudo systemctl
+        abbr -a syses sudo systemctl enable --now
+        abbr -a sysds sudo systemctl disable --now
+        abbr -a sysed sudo systemctl edit
+        abbr -a sysdr sudo systemctl daemon-reload
+        abbr -a log journalctl -u
+    end
+    abbr -a json python3 -m json.tool --no-ensure-ascii
+
+    # Editor
+    if command -q nvim
+        alias vim=nvim
+        set -gx EDITOR nvim
+    else
+        set -gx EDITOR vim
+    end
+
+    # WSL
+    if uname -a | string match -eq WSL2
+        alias dropcache='echo 1 | sudo tee /proc/sys/vm/drop_caches > /dev/null'
+    end
 end
